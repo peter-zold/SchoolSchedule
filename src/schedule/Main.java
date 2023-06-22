2f151f391a300a6d570f8af7250a31e790bec160
Roland <korolesz@gmail.com>
2023-06-22 23:24:57 +0200

    git próba

--- src/schedule/Main.java
+++ src/schedule/Main.java
@@ -13,7 +13,7 @@ import java.util.List;
 //import schedule.displayandtxtanddatabase.RoomArrangementTxtMaker;
 //import schedule.displayandtxtanddatabase.TimetableDataInput;
 
-
+//próba
 //FELADATOK
 
 //Simon ----------------------------------------------

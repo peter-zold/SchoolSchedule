310a3ff8d4effd42ab918662adc80b1b79c89574
Zöld Péter <zold.peter10@gmail.com>
2023-07-03 14:41:01 +0200

    Csoportfelosztás

---
+++ src/schedule/GeneticAlgorithm2.java
@@ -0,0 +1,232 @@
+package schedule;
+
+import schedule.data.Classes;
+import schedule.data.Lesson;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * The GeneticAlgorithm class is our main abstraction for managing the
+ * operations of the genetic algorithm. This class is meant to be
+ * problem-specific, meaning that (for instance) the "calcFitness" method may
+ * need to change from problem to problem.
+ * <p>
+ * This class concerns itself mostly with population-level operations, but also
+ * problem-specific operations such as calculating fitness, testing for
+ * termination criteria, and managing mutation and crossover operations (which
+ * generally need to be problem-specific as well).
+ * <p>
+ * Generally, GeneticAlgorithm might be better suited as an abstract class or an
+ * interface, rather than a concrete class as below. A GeneticAlgorithm
+ * interface would require implementation of methods such as
+ * "isTerminationConditionMet", "calcFitness", "mutatePopulation", etc, and a
+ * concrete class would be defined to solve a particular problem domain. For
+ * instance, the concrete class "TravelingSalesmanGeneticAlgorithm" would
+ * implement the "GeneticAlgorithm" interface. This is not the approach we've
+ * chosen, however, so that we can keep each chapter's examples as simple and
+ * concrete as possible.
+ *
+ * @author bkanber
+ */
+
+public class GeneticAlgorithm2 {
+
+    protected int tournamentSize;
+    private int populationSize;
+    /**
+     * Mutation rate is the fractional probability than an individual gene will
+     * mutate randomly in a given generation. The range is 0.0-1.0, but is
+     * generally small (on the order of 0.1 or less).
+     */
+    private double mutationRate;
+    /**
+     * Crossover rate is the fractional probability that two individuals will
+     * "mate" with each other, sharing genetic information, and creating
+     * offspring with traits of each of the parents. Like mutation rate the
+     * rance is 0.0-1.0 but small.
+     */
+    private double crossoverRate;
+    /**
+     * Elitism is the concept that the strongest members of the population
+     * should be preserved from generation to generation. If an individual is
+     * one of the elite, it will not be mutated or crossover.
+     */
+    private int elitismCount;
+
+
+    public GeneticAlgorithm2(int populationSize, double mutationRate, double crossoverRate, int elitismCount, int tournamentSize) {
+        this.populationSize = populationSize;
+        this.mutationRate = mutationRate;
+        this.crossoverRate = crossoverRate;
+        this.elitismCount = elitismCount;
+        this.tournamentSize = tournamentSize;
+
+    }
+
+    /**
+     * Initialize population
+     *
+     * @param allTheClasses The classes arrayList from input data
+     * @return population The initial population generated
+     */
+    public Population2 initPopulation(List<Classes> allTheClasses) {
+        // Initialize population
+        Population2 population = new Population2(this.populationSize, allTheClasses);
+        return population;
+    }
+
+
+    /**
+     * Check if population has met termination condition
+     *
+     * Hoping for perfect result, termination condition is a fitness value of 1.0
+     *
+     * @param population
+     * @return boolean True if termination condition met, otherwise, false
+     */
+    public boolean isTerminationConditionMet(Population2 population) {
+        for (Individual2 individual : population.getIndividuals()) {
+            if (individual.getFitness() == 1) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Selects parent for crossover using tournament selection
+     * <p>
+     * Tournament selection works by choosing N random individuals, and then
+     * choosing the best of those.
+     *
+     * @param population
+     * @return The individual selected as a parent
+     */
+    public Individual2 selectParent(Population2 population) {
+        // Create tournament
+        Population2 tournament = new Population2(this.tournamentSize);
+
+        // Add random individuals to the tournament
+        population.shuffle();
+        for (int i = 0; i < this.tournamentSize; i++) {
+            Individual2 tournamentIndividual = population.getIndividual(i);
+            tournament.setIndividual(i, tournamentIndividual);
+        }
+
+        // Return the best
+        return tournament.getFittest(0);
+    }
+
+    /**
+     * Apply crossover to population
+     *
+     * Crossover, more colloquially considered "mating", takes the population
+     * and blends individuals to create new offspring. Offspring may have better fittnes than its parents.
+     * It's up to testing to decide wether random crossover or guided crossover is better
+     *
+     * This method considers both the GeneticAlgorithm instance's crossoverRate
+     * and the elitismCount.
+     *
+     * @param population The population to apply crossover to
+     * @return The new population
+     */
+    public Population2 crossoverPopulation(Population2 population) {
+        // Create new population
+        Population2 newPopulation = new Population2(population.size());
+
+        // Loop over current population by fitness
+        for (int populationIndex = 0; populationIndex < population.size(); populationIndex++) {
+            Individual2 parent1 = population.getFittest(populationIndex);
+
+            // Apply crossover to this individual?
+            if (this.crossoverRate > Math.random() && populationIndex > this.elitismCount) {
+
+                // Find second parent
+                Individual2 parent2 = selectParent(population);
+
+                // Initialize 10 offsprings. Validity of number 10 is up to more testing
+                Population2 offspringsPop = new Population2(10);
+                for (int i = 0; i < 10; i++) {
+                    Individual2 offspring = Individual2.breedOffspring(parent1, parent2);
+                    offspring.setFitness(offspring.calcFitness());
+                    offspringsPop.setIndividual(i, offspring);
+                }
+                // get fittest offspring
+                Individual2 fittestOffspring = offspringsPop.getFittest(0);
+                // if offspring is fitter than parent
+                if(fittestOffspring.getFitness() > parent1.getFitness()) {
+                    // Add offspring to new population
+                    newPopulation.setIndividual(populationIndex, fittestOffspring);
+                }
+                // if not fitter, parent remains
+                else {
+                    newPopulation.setIndividual(populationIndex, parent1);
+                }
+
+            } else {
+                // Add individual to new population without applying crossover
+                newPopulation.setIndividual(populationIndex, parent1);
+            }
+        }
+
+        return newPopulation;
+    }
+
+    /**
+     * Apply mutation to population
+     * <p>
+     * Mutation affects individuals rather than the population. We look at each
+     * individual in the population, and if they're lucky enough,
+     * apply some randomness to their chromosome by swapping classes.
+     * This method will consider the GeneticAlgorithm instance's mutationRate
+     * and elitismCount
+     *
+     * @param population The population to apply mutation to
+     * @return The mutated population
+     */
+    public Population2 mutatePopulation(Population2 population) {
+        // Initialize new population
+        Population2 newPopulation = new Population2(this.populationSize);
+        // Loop over current population by fitness
+        for (int populationIndex = 0; populationIndex < population.size(); populationIndex++) {
+            // choose and individual and clone it
+            Individual2 individualOriginal = population.getFittest(populationIndex);
+            Individual2 individual = new Individual2(individualOriginal);
+
+            // apply mutation to the clone
+            if (populationIndex > this.elitismCount) {
+                for (int classIndex = 0; classIndex < individual.getNumOfClasses(); classIndex++) {
+                    // Does this class timetable need mutation?
+                    if (this.mutationRate > Math.random()) {
+                        // Get two colliding classes swap
+                        individual.mutateTwoCollisions(classIndex);
+                    }
+                    if (this.mutationRate > Math.random()) {
+                        // Get one colliding class swap with a random
+                        individual.mutateOneCollision(classIndex);
+                    }
+                    if (this.mutationRate > Math.random()*2) {
+                        // Get two random classes swap
+                        individual.mutateRandom(classIndex);
+                    }
+                }
+                // if mutated clone is fitter, swap it with the original
+                if (individual.getFitness() >= individualOriginal.getFitness()) {
+                    // Add individual to population
+                    newPopulation.setIndividual(populationIndex, individual);
+                } else {
+                    // if not fitter, original remains
+                    newPopulation.setIndividual(populationIndex, individualOriginal);
+                }
+            }
+            else {
+                // if decided to not mutate, original remains
+                newPopulation.setIndividual(populationIndex, individualOriginal);
+            }
+        }
+        // Return population after mutation process
+        return newPopulation;
+    }
+}
---
+++ src/schedule/Individual2.java
@@ -0,0 +1,285 @@
+package schedule;
+
+
+import schedule.data.Classes;
+import schedule.data.Lesson;
+
+import schedule.data.TimeTable;
+import schedule.data.TimeTable2;
+
+import java.util.*;
+
+
+public class Individual2 {
+
+    private List<Lesson>[][] timetable;
+    private double fitness = -1;
+    private int numOfClasses;
+    private ArrayList<Integer>[] candidatesForMutation;  //int index is ClassID
+
+
+    /**
+     * Create individual from a timetable and auto-calculate other fields
+     *
+     * @param timetable
+     *            The number of individuals in the population
+     */
+
+    public Individual2(List<Lesson>[][] timetable) {
+        this.timetable = timetable;
+        this.numOfClasses = timetable.length;
+        this.candidatesForMutation = new ArrayList[this.numOfClasses];
+        for (int i = 0; i < this.numOfClasses; i++) {
+            this.candidatesForMutation[i] = new ArrayList<Integer>();
+        }
+        this.fitness = calcFitness();
+    }
+
+    /**
+     * Clone an individual
+     *
+     * @param individual
+     *            The number of individuals in the population
+     */
+    public Individual2(Individual2 individual) {
+        this.timetable = individual.getTimetable();
+        this.numOfClasses = individual.getNumOfClasses();
+        this.candidatesForMutation = new ArrayList[this.numOfClasses];
+        for (int i = 0; i < this.numOfClasses; i++) {
+            this.candidatesForMutation[i] = new ArrayList<Integer>();
+        }
+        this.fitness = individual.calcFitness();
+    }
+
+    /**
+     * Create an individual from scanned data for first generation
+     *
+     * @param allClasses
+     *            ArrayList containing instances of all the classes
+     */
+
+    public Individual2(List<Classes> allClasses) {
+        List<Lesson>[][] tempTimeTable = new ArrayList[allClasses.size()][];
+        for (int i = 0; i < allClasses.size(); i++) {
+            tempTimeTable[i] = TimeTable2.createRandomTimeTable(allClasses.get(i));
+        }
+        this.timetable = tempTimeTable;
+        this.numOfClasses = allClasses.size();
+        this.candidatesForMutation = new ArrayList[numOfClasses];
+        for (int i = 0; i < numOfClasses; i++) {
+            this.candidatesForMutation[i] = new ArrayList<Integer>();
+        }
+        this.fitness = calcFitness();
+    }
+
+    /**
+     * Create an individual from scanned data for first generation
+     *
+     * @param parent1
+     *            Mother parent
+     * @param parent2
+     *            Father parent
+     */
+    public static Individual2 breedOffspring(Individual2 parent1, Individual2 parent2) {
+        List<Lesson>[][] offspringTimeTable = new ArrayList[parent1.getNumOfClasses()][45];
+
+        // Loop over genome for all the offspring candidates
+        for (int classIndex = 0; classIndex < parent1.getNumOfClasses(); classIndex++) {
+            // Use half of parent1's genes and half of parent2's genes
+            if (0.5 > Math.random()) {
+                offspringTimeTable[classIndex] = parent1.getClassTimetable(classIndex);
+            } else {
+                offspringTimeTable[classIndex] = parent2.getClassTimetable(classIndex);
+            }
+        }
+        return new Individual2(offspringTimeTable);
+    }
+
+    /**
+     * Get number of classes
+     *
+     */
+
+    public int getNumOfClasses() {
+        return numOfClasses;
+    }
+
+    /**
+     * Clone individuals timetable
+     *
+     */
+    public List<Lesson>[][] getTimetable() {
+        List<Lesson>[][] newTimeTable = new ArrayList[this.numOfClasses][45];
+        for (int i = 0; i < this.numOfClasses; i++) {
+            for (int j = 0; j < 45; j++) {
+                newTimeTable[i][j]=this.timetable[i][j];
+            }
+        }
+        return newTimeTable;
+    }
+
+    /**
+     * Get individual's fitness value
+     *
+     */
+    public double getFitness() {
+        return this.fitness;
+    }
+
+    /**
+     * Set individual's fitness value manually
+     *
+     * @param fitness
+     *            Fitness value to set
+     */
+    public void setFitness(double fitness) {
+        this.fitness = fitness;
+    }
+
+    /**
+     * Calculate and set individual's fitness
+     * Get candidates for mutation
+     *
+     */
+    public double calcFitness() {
+        for (ArrayList<Integer> studentClass: candidatesForMutation) {
+            studentClass.clear();
+        }
+        int clashes = 0;
+        // clashes for same teacher in same timeslot
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < timetable[0].length; i++) {
+            for (int j = 0; j < timetable.length; j++) {
+                for (int k = 0; k < timetable[j][i].size(); k++) {
+                    if (timetable[j][i].get(k).getValueOfFreeness() == 0 && !set.add(timetable[j][i].get(k).getTeacher().getName())) {
+                    candidatesForMutation[j].add(i);
+                    clashes++;
+                    }
+                }
+            }
+            set.clear();
+        }
+        // clashes for same lesson in same day
+        Set<String> set2 = new HashSet<>();
+        for (int i = 0; i < timetable.length; i++) {
+            for (int j = 0; j < timetable[i].length; j++) {
+                if (j % 9 == 0) {
+                    set2.clear();
+                }
+                for (int k = 0; k < timetable[i][j].size(); k++) {
+                    if (timetable[i][j].get(k).getValueOfFreeness() == 0 && !set2.add(timetable[i][j].get(k).getNameOfLesson())) {
+                        candidatesForMutation[i].add(j);
+                        clashes++;
+                    }
+                }
+            }
+        }
+        // calculate fitness
+        double calculatedFitness = (double) 1 / (double) (1 + clashes);
+        // set fitness
+        setFitness(calculatedFitness);
+        return calculatedFitness;
+    }
+
+    /**
+     * Mutate individual's timetable by swapping colliding classes
+     *
+     * @param classID
+     *            Which classes' timetable to mutate
+     */
+
+    public void mutateTwoCollisions(int classID) {
+        // how many collisions in a class timetable
+        int collNum = candidatesForMutation[classID].size();
+        if (collNum >= 2) {
+            // choose two random indexes, and get Lesson day and hour location indexes from them
+            int i = (int) Math.floor(Math.random() * collNum);
+            int j;
+            do {
+                j = (int) Math.floor(Math.random() * collNum);
+            } while (i == j);
+            int dayHour1 = candidatesForMutation[classID].get(i);
+            int dayHour2 = candidatesForMutation[classID].get(j);
+
+            // swap these lessons, if they are not the same
+            if (!timetable[classID][dayHour1].equals(timetable[classID][dayHour2])) {
+                List<Lesson> temp = timetable[classID][dayHour2];
+                timetable[classID][dayHour2] = timetable[classID][dayHour1];
+                timetable[classID][dayHour1] = temp;
+            }
+        }
+    }
+
+    /**
+     * Mutate individual's timetable by swapping one colliding classe
+     * with a random class
+     *
+     * @param classID
+     *            Which classes' timetable to mutate
+     */
+    public void mutateOneCollision(int classID) {
+        // How many collisions in a class timetable
+        int collNum = candidatesForMutation[classID].size();
+        if (collNum >= 1) {
+            // choose two random indexes, and get Lesson day and hour location indexes from them
+            int i = (int) Math.floor(Math.random() * collNum);
+            int dayHour1 = candidatesForMutation[classID].get(i);
+            int dayHour2 = (int) Math.floor(Math.random() * timetable[classID].length);
+
+            // swap these lessons, if they are not the same
+            if (!timetable[classID][dayHour1].equals(timetable[classID][dayHour2]) && timetable[classID][dayHour2].get(0).getValueOfFreeness() == 0) {
+                List<Lesson> temp = timetable[classID][dayHour2];
+                timetable[classID][dayHour2] = timetable[classID][dayHour1];
+                timetable[classID][dayHour1] = temp;
+            }
+        }
+    }
+
+    /**
+     * Mutate individual's timetable by swapping random classes
+     *
+     * @param classID
+     *            Which classes' timetable to mutate
+     */
+    public void mutateRandom(int classID) {
+        // choose two random indexes, and get Lesson day and hour location indexes from them
+        int dayHour1 = (int) Math.floor(Math.random() * timetable[classID].length);
+        int dayHour2 = (int) Math.floor(Math.random() * timetable[classID].length);
+
+        // swap these lessons, if they are not the same
+        if (!timetable[classID][dayHour1].equals(timetable[classID][dayHour2])
+                // Only if free period does not blend with other group's lessons
+                && timetable[classID][dayHour1].get(0).getValueOfFreeness() == 0
+                && timetable[classID][dayHour2].get(0).getValueOfFreeness() == 0) {
+            List<Lesson> temp = timetable[classID][dayHour2];
+            timetable[classID][dayHour2] = timetable[classID][dayHour1];
+            timetable[classID][dayHour1] = temp;
+        }
+    }
+
+    /**
+     * Clone a class' timetable
+     *
+     * @param classID
+     *            Which classes' timetable to clone
+     */
+    public List<Lesson>[] getClassTimetable(int classID) {
+        List<Lesson>[] classTimetable = new ArrayList[45];
+        for (int i = 0; i < classTimetable.length; i++) {
+            classTimetable[i] = this.timetable[classID][i];
+        }
+        return classTimetable;
+        //return this.timetable[classID];
+    }
+
+
+
+
+}
+
+
+
+
+
+
+
